64548c6af8817f13afd7a687f29a813ca069ee29
matthewPapke <matt.papke@gmail.com>
2019-02-02 23:28:11 -0500

    return countrty on backekend

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.template import loader
+import json
+from django.views.decorators.csrf import csrf_exempt
 from . import services
 
 
@@ -9,10 +11,13 @@ def index(request):
 	context = {}
 	return HttpResponse(template.render(context, request))
 
+# given {lat,lng}, return the country and the price of Bitcoin
+@csrf_exempt
 def get_btc_price(request):
-	print("request received")
-	# TODO get lat,lng from request
-	country_code = services.get_country_from_coordinates(1.0, 1.0)
+	body_unicode = request.body.decode('utf-8')
+	body = json.loads(body_unicode)
+	country = services.get_country_from_coordinates(float(body['lat']), lng = float(body['lng']))
+	country_code = country['country_code']
 	currency_code = services.get_country_currency(country_code)
 	btc_price = services.get_btc_price(currency_code)
-	return JsonResponse({'btc_price': btc_price})
+	return JsonResponse({'btc_price': btc_price, 'country_name': country['country_name']})
