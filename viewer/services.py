790caaeb258761e6b80ee99af8ebeb127e6f6c10
matthewPapke <matt.papke@gmail.com>
2019-02-16 21:13:26 -0500

    updated exception handling

--- viewer/services.py
+++ viewer/services.py
@@ -2,25 +2,43 @@ import reverse_geocoder as rg
 import pycountry as pyc
 from forex_python.bitcoin import BtcConverter
 from babel import numbers
+from . import exceptions as e
+
+
+def get_location_btc_price_info(lat, lng):
+	country = get_country_from_coordinates(lat,lng)
+	if country is None:
+		raise e.LocationBtcPriceException('Unable to reverse geocode coordinates')
+		# return {'failure_message': 'Unable to reverse geocode coordinates'}
+	
+	currency = pyc.currencies.get(numeric=country.numeric)
+	if currency is None:
+		raise e.LocationBtcPriceException('Unable to get currency information about {}'.format(country.name))
+		# return {'failure_message': 'Unable to get currency information about country'}
+	
+	# get currency and BTC price conversion for given country
+	btc_price = get_btc_price(currency.alpha_3)
+	if btc_price is None:
+		raise e.LocationBtcPriceException('BTC exchange rate is not available for currency {}'.format(currency.name))
+		# return {'failure_message': 'BTC exchange rate is not available for currency'}
+
+	# return location, currency, and btc information
+	return {'btc_price': btc_price, 'currency_name': currency.name, 'country_name': country.name}
+
 
 # reverse geocode {lat,lng}
 def get_country_from_coordinates(lat, lng):
-	coordinates = (lat,lng)
-	country_results = rg.search(coordinates)
+	country_results = rg.search((lat,lng))
 	country = country_results[0]
 	country_code = country.get('cc')
-	country_name = pyc.countries.get(alpha_2=country_code).name
-	return {'country_name': country_name, 'country_code': country_code}
+	return pyc.countries.get(alpha_2=country_code)
 
-# get currency from country code
-def get_country_currency(country_code):
-	country = pyc.countries.get(alpha_2=country_code)
-	currency = pyc.currencies.get(numeric=country.numeric)
-	return {'alpha3': currency.alpha_3, 'name': currency.name}
-
+	
 # convert currency to BTC
 def get_btc_price(currency_code):
 	b = BtcConverter() 
 	price = b.get_latest_price(currency_code)
-	formatted_price = numbers.format_currency(price, currency_code, locale='en')
-	return formatted_price
+	if price is not None:
+		return numbers.format_currency(price, currency_code, locale='en')
+	else:
+		return None
--- viewer/views.py
+++ viewer/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.http import HttpResponseServerError
 from django.template import loader
+
 import json
 from django.views.decorators.csrf import csrf_exempt
 from . import services
+from . import exceptions as e
 
 
 def index(request):
@@ -13,15 +16,18 @@ def index(request):
 
 @csrf_exempt
 def get_btc_price(request):
-	# parse {lat,lng} from request
-	body_unicode = request.body.decode('utf-8')
-	body = json.loads(body_unicode)
-	lat = float(body['lat'])
-	lng = float(body['lng'])
-	# get country from {lat,lng}
-	country = services.get_country_from_coordinates(lat, lng)
-	country_code = country['country_code']
-	# get currency and BTC price conversion for given country
-	currency = services.get_country_currency(country_code)
-	btc_price = services.get_btc_price(currency['alpha3'])
-	return JsonResponse({'btc_price': btc_price, 'currency_name': currency['name'], 'country_name': country['country_name']})
+	try:
+		# parse {lat,lng} from request
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+		lat = float(body['lat'])
+		lng = float(body['lng'])
+		# get btc price info from {lat,lng}
+		result = services.get_location_btc_price_info(lat, lng)
+		return JsonResponse(result)
+
+	except e.LocationBtcPriceException as ex:
+		result = {'failure_message': str(ex)}
+		return JsonResponse(result)
+
+
