386150fa72e0146caa06deff36e979e49e304246
matthewPapke <matt.papke@gmail.com>
2019-02-19 21:05:59 -0500

    improve exception handling, change endpoint to accept GET requests

--- viewer/views.py
+++ viewer/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.http import HttpResponseServerError
+from django.http import HttpResponseBadRequest
 from django.template import loader
-
 import json
-from django.views.decorators.csrf import csrf_exempt
 from . import services
 from . import exceptions as e
 
@@ -14,20 +13,25 @@ def index(request):
 	context = {}
 	return HttpResponse(template.render(context, request))
 
-@csrf_exempt
 def get_btc_price(request):
 	try:
+		if request.method != 'GET':
+			return HttpResponseNotAllowed('This is a GET-only endpont')
+
 		# parse {lat,lng} from request
-		body_unicode = request.body.decode('utf-8')
-		body = json.loads(body_unicode)
-		lat = float(body['lat'])
-		lng = float(body['lng'])
+		lat = float(request.GET.get('lat', ''))
+		lng = float(request.GET.get('lng', ''))
+
 		# get btc price info from {lat,lng}
 		result = services.get_location_btc_price_info(lat, lng)
 		return JsonResponse(result)
 
+	except ValueError as ex:
+		return HttpResponseBadRequest("Invalid lat and lng parameters in request")
+		print(str(ex))
+
 	except Exception as ex:
 		print(str(ex))
-		return HttpResponseServerError(str(ex))
+		return HttpResponseServerError()
 
 
